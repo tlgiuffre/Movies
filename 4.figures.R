9fa992b1e7a9eda2307b8967b414941929d0b829
Sofia Graves <79480336+COOKIESROK@users.noreply.github.com>
2021-12-06 13:41:38 -0800

    Update 4.figures.R

--- 4.figures.R
+++ 4.figures.R
@@ -1,3 +1,16 @@
+working_directory <- getwd()
+
+# === 1) distribution of the average rating per user ===========================
+
+# Step 1: Call the pdf command to start the plot
+pdf(file = paste(working_directory, "/Distribution of average rating per user.pdf"),   
+    # The directory you want to save the file in
+    width = 4, # The width of the plot in inches
+    height = 4) # The height of the plot in inches
+
 # displaying distribution of the average rating per user
-qplot(average_ratings, fill=I("steelblue"), col=I("red")) +
+qplot(average_ratings, fill=I("#0145ac"), col=I("black")) +
   ggtitle("Distribution of the average rating per user")
+
+# Step 3: Run dev.off() to create the file!
+dev.off()
