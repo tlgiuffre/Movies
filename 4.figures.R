b7e5606634519c8e3b753bb25b625368d4c1c764
Sofia Graves <79480336+COOKIESROK@users.noreply.github.com>
2021-12-06 13:50:09 -0800

    Update 4.figures.R

--- 4.figures.R
+++ 4.figures.R
@@ -8,9 +8,18 @@ pdf(file = paste(working_directory, "/Distribution of average rating per user.pd
     width = 4, # The width of the plot in inches
     height = 4) # The height of the plot in inches
 
-# displaying distribution of the average rating per user
+# Step 2: displaying distribution of the average rating per user
 qplot(average_ratings, fill=I("#0145ac"), col=I("black")) +
   ggtitle("Distribution of the average rating per user")
 
 # Step 3: Run dev.off() to create the file!
 dev.off()
+
+# === 2) bar plot of total view of popular movies ==============================
+
+# Step 1: Call the pdf command to start the plot
+pdf(file = paste(working_directory, "/Distribution of average rating per user.pdf"),   
+    # The directory you want to save the file in
+    width = 4, # The width of the plot in inches
+    height = 4) # The height of the plot in inches
+
